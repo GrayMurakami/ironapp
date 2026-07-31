fb34044a63b109ccb1564d7286d927db5b6583a2
Gray <digrayman8@gmail.com>
2026-07-31 23:42:44 +0900

    feat: start write LoginView.vue, import, variables

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,18 @@
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/auth';
+
+const router = useRouter();
+const auth = useAuthStore();
+
+const mode = ref('signin');
+const email = ref('');
+const password = ref('');
+const gender = ref('male');
+const isLoading = ref(false);
+const error = ref('');
+
 
 </script>
 
