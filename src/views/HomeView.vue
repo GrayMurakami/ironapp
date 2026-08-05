21ccbfd553ff474358e576bff8180fc0b853b180
Gray <digrayman8@gmail.com>
2026-08-05 19:43:43 +0900

    feat: HomeView.vue script logic add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup>
+import { onMounted, computed } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useAuthStore } from '@/stores/auth'
+import { useProgramStore } from '@/stores/program'
 
+const auth = useAuthStore();
+const program = useProgramStore();
+
+const { appNameDemo } = storeToRefs(auth);
+const { loaded, error, hasDays } = storeToRefs(program);
+
+const showOnboarding = computed(() => loaded.value && !hasDays.value);
+
+onMounted(() => {
+  program.fetchDays();
+});
 </script>
 
 <template>
-  <div>Hi</div>
+
 </template>
 
 <style scoped>
